9593b3a4613ad36730b0d25d3e187139d68e2719
Pedro Henrique Ribeiro de Assis <pedroh2306@gmail.com>
2011-03-30 00:41:50 -0300

    New Teleporter_receiver class with convertion methods and inclusion of two handlers for creating and deleting objects

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,4 +1,86 @@
+require "pp"
+
+class Logger
+  def self.success
+    puts "\n\n"
+    puts "(Teleport)"
+    puts "  New object teleported!"
+    puts ""
+  end
+  
+  def self.fail
+    puts "\n\n"
+    puts "(Teleport)"
+    puts "  Fail trying to save a new object teleported!"
+    puts ""
+  end    
+end
+
+class Teleport_receiver
+  #hash: {satelite_attribute => teleport_attribute}
+  #TODO: this mapping has to be created automatically
+  @@mapping = {
+    "occupation" => "occupation",
+    "address" => "address",
+    "name" => "name",
+    "phone" => "phone"}
+
+  #Satellite param
+  @@key = "name"
+    
+  #TODO: handle nil cases
+  def self.mapper(val)
+    return @@mapping[val]
+  end
+
+  def self.get_search_key(params)
+    return {@@key => params["person"][@@mapping[@@key]]}
+  end
+
+  def self.convert_params(params, target)
+    info = params["person"]
+    hash = target.attributes
+    hash.each do |key,value|
+      target[key] = info[mapper(key)]
+    end
+    return target
+  end
+
+  def self.convert_object(params, object)
+    convert_params(params, object)
+    return object
+  end
+
+  def self.convert_hash(params, object)
+    convert_params(params, object)
+    hash = object.attributes
+    hash.delete_if {|k,v| v == nil}
+    return hash
+  end
+end
+
 class CandidatesController < ApplicationController
+  def teleport_destroy
+    key = Teleport_receiver.convert_hash(params, Candidate.new)
+    @candidate = Candidate.find(:first, :conditions => key)
+    @candidate.destroy unless @candidate == nil
+
+    render :nothing => true
+  end
+
+  def teleport_create
+    @candidate = Candidate.new
+    Teleport_receiver.convert_object(params, @candidate)
+
+    if @candidate.save
+      Logger.success
+    else
+      Logger.fail
+    end
+
+    render :nothing => true
+  end
+
   # GET /candidates
   # GET /candidates.xml
   def index
